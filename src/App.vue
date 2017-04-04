dbc3e348427cfe6cdffc1110e1925c5b4341a48d
zhangruinian <946758095@qq.com>
2017-04-04 16:19:31 +0800

    后端数据交互完成，可微调

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,56 @@
 <template>
-    <div id="app">
-        <h2>请把图片左上角红小方块拖动到缺角出，尽量保证准确性</h2>
-        <ul>
-            <li v-for="(img, index) in imgs" :key="index">
-                <span @mousedown.prevent="down" @mousemove.prevent="move" @mouseup="up(index)"></span>
-                <img :src="img" alt="">
-                <a>确定</a>
-            </li>
-        </ul>
-        <button @click="submit">提交</button>
-    </div>
+	<div id="app">
+		<h2>请把图片中的红色小方块拖动到缺角出，尽量保证准确性</h2>
+		<h5>提示：可使用键盘的上下左右方向键进行微调，Enter键可进行提交</h5>
+		<div class="img-wrap">
+			<span @mousedown.prevent="down" @mousemove.prevent="move" @mouseup="up()" id="captcha"></span>
+			<img :src="imgUrl">
+		</div>
+		<table>
+			<tr>
+				<td>X坐标</td>
+				<td>Y坐标</td>
+			</tr>
+			<tr>
+				<td>{{position.x_pos}}</td>
+				<td>{{position.y_pos}}</td>
+			</tr>
+		</table>
+		<button @click="submit">提交</button>
+	</div>
 </template>
-
 <script>
-    import c1 from "./assets/imgs/1.jpg"
-    import c2 from "./assets/imgs/2.jpg"
-    import c3 from "./assets/imgs/3.jpg"
-    import c4 from "./assets/imgs/4.jpg"
-
-
+    import axios from "axios"
     export default {
         name: 'app',
         data(){
             return{
-                select:null,
                 isDown:false,
                 ObjLeft:0,
                 ObjTop:0,
                 posX:0,
                 posY:0,
-                imgs:[
-                    c1,c2,c3,c4
-                ],
-                positions:[],
-                position:{}
-            }
+                position:{
+                },
+                id:"",
+                imgUrl:"",
+                apiUrl:"http://api.apiapp.cc/"
+           }
+        },
+        computed:{
         },
         methods:{
+            getQueryValue(name, url){
+                if (!url) {
+                    url = window.location.href;
+                }
+                name = name.replace(/[\[\]]/g, "\\$&");
+                var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                    results = regex.exec(url);
+                if (!results) return null;
+                if (!results[2]) return '';
+                return decodeURIComponent(results[2].replace(/\+/g, " "));
+            },
             mousePosition(event){
                 return{
                     x:event.pageX,
@@ -50,14 +64,8 @@
                 this.posX = parseInt(this.mousePosition(event).x);
                 this.posY = parseInt(this.mousePosition(event).y);
             },
-            up(index){
-                this.select = index;
+            up(){
                 this.isDown = false;
-               /* console.log(index);
-                if ( this.select){
-                    this.positions.splice(this.select,1)
-                }*/
-                this.positions.push(this.position);
             },
             move(event){
                 if (this.isDown) {
@@ -65,12 +73,9 @@
                     var y = parseInt(parseInt(this.mousePosition(event).y) - this.posY + this.ObjTop);
                     var w = 320 - event.target.clientWidth;
                     var h = 100 - event.target.clientHeight;
-                    /*var w = document.documentElement.clientWidth - event.target.offsetWidth;
-                    var h = document.documentElement.clientHeight - event.target.offsetHeight;*/
                     if (x < 0) {
                         x = 0
                     } else if (x > w) {
-
                         x = w
                     };
                     if (y < 0) {
@@ -81,60 +86,123 @@
                     event.target.style.left = x + 'px';
                     event.target.style.top = y + 'px';
                     this.position ={
-                        x:x,
-                        y:y
+                        x_pos:x || 0,
+                        y_pos:y || 0,
+                        id:this.id,
+                        token:this.getQueryValue("token")
                     }
                 }
             },
             submit(){
-                console.table(this.positions);
-            }
+                axios.post(this.apiUrl+ 'slide/update-slide-ans/', {
+					...this.position
+			    }).then(
+				    ()=>{
+				        this.getImageData()
+				    }
+			    )
+			},
+			getImageData(){
+				axios.get(this.apiUrl+ "slide/read-slide-pic/",{
+                params:{
+                    token:this.getQueryValue("token"),
+                    user:1,
+                    }
+	            }).then((res)=>{
+	               this.id = res.data.data.id;
+	              this.imgUrl = 'http://geetest.f3322.net:28079/capt-163-slide/__'+res.data.data.bg_name + '__' +res.data.data.rand_tag+'.jpg'
+	            }).catch((error)=>{
+	                console.log(error)
+	            })
+			}
+		},
+        components: {},
+        created(){
+			this.getImageData();
         },
-        components: {}
+        mounted(){
+            let Ecaptcha = document.getElementById("captcha");
+            document.onkeydown=(event) => {
+               var e = event || window.event;
+               if(e && e.keyCode==13){//enter 提交
+					this.submit()
+				}
+				if(e && e.keyCode==37){//左箭头
+					Ecaptcha.style.left = Ecaptcha.offsetLeft - 1 + "px"
+					if(Ecaptcha.offsetLeft <= 0){
+						Ecaptcha.style.left  = "0px"
+					}
+				}
+				if(e && e.keyCode==38){//上
+					Ecaptcha.style.top = Ecaptcha.offsetTop - 1 + "px"
+					if(Ecaptcha.offsetTop <= 0){
+						Ecaptcha.style.top  = "0px"
+					}
+				}
+				if(e && e.keyCode==39){//右
+					Ecaptcha.style.left = Ecaptcha.offsetLeft + 1 + "px"
+					if(Ecaptcha.offsetLeft >= 272){
+						Ecaptcha.style.left  = "272px"
+					}
+				}
+				if(e && e.keyCode==40){//下
+					Ecaptcha.style.top = Ecaptcha.offsetTop + 1 + "px"
+					if(Ecaptcha.offsetTop >= 62){
+						Ecaptcha.style.top  = "62px"
+					}
+				}
+				this.position.x_pos = Ecaptcha.offsetLeft;
+				this.position.y_pos = Ecaptcha.offsetTop;
+			};
+        },
+
     }
 </script>
-
 <style scoped lang=less rel="stylesheet/less">
-    #app {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        color: #2c3e50;
-        margin-top: 60px;
-        ul{
-            width: 800px;
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-            li{
-                list-style: none;
-                margin-right: 20px;
-                position: relative;
-                span{
-                    position: absolute;
-                    display: inline-block;
-                    width: 48px;
-                    height: 38px;
-                    /*background-color: red;*/
-                    background-color: rgba(224,42,30,0.8);
-                }
-                a{
-                    display: inline-block;
-                    text-align: center;
-                    width: 50px;
-                    height: 30px;
-                    line-height: 30px;
-                    background-color: #000;
-                    color: #fff;
+	#app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		color: #2c3e50;
+		margin-top: 60px;
+		.img-wrap {
+			width: 320px;
+			display: flex;
+			flex-wrap: wrap;
+			position: relative;
+			img {
+				width: 320px;
+				height: 100px;
+			}
+			span {
+				position: absolute;
+				display: inline-block;
+				width: 48px;
+				height: 38px;
+				background-color: rgba(224, 42, 30, 0.9);
+				&:hover{
+					cursor: move;
+				}
+			}
+		}
+		button {
+			width: 100px;
+			height: 30px;
+			margin-top: 20px;
+			&:hover {
+				cursor: pointer;
+			}
+		}
+        table{
+            tr{
+                text-align: center;
+                td{
+
                 }
             }
         }
-        button{
-            width: 100px;
-            height: 30px;
-        }
-    }
+	}
 </style>
